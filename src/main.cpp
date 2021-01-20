c2e697f73df80dd6a8b886fccc776fc364f05111
TechPlayz <69027236+TechPlayz@users.noreply.github.com>
2021-01-20 10:21:55 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
     float op;
     float A;
     float B;
-	int enter;
+    int enter;
     cout << "Welcome to Tech Playz calculator app \n";
     cout << "Which operation do you want \n";
     cout << "1-Addition \n"<<"2-Subtraction \n"<<"3-Multiplication \n"<<"4-Division \n";
